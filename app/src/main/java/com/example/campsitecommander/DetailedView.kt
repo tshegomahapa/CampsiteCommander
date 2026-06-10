03b6e0d1e666bdd3e10bc6d3b729bfb5db6d1619
tshegomahapa <tshegofatsomahapa19@gmail.com>
2026-06-10 11:52:24 +0200

    more alignment added

--- app/src/main/java/com/example/campsitecommander/DetailedView.kt
+++ app/src/main/java/com/example/campsitecommander/DetailedView.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.campsitecommander.ui.theme.CampsiteCommanderTheme
@@ -21,14 +24,23 @@ class DetailedView : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CampsiteCommanderTheme {
-                Button(
-                    onClick = {
-                        //Goes back to Main Screen.
-                        val intent = Intent(this@DetailedView, MainActivity::class.java)
-                        startActivity(intent)
+                Column(
+                    modifier =
+                        Modifier.fillMaxSize(),
+                    verticalArrangement =
+                        Arrangement.Center,
+                    horizontalAlignment =
+                        Alignment.CenterHorizontally
+                ){
+                    Button(
+                        onClick = {
+                            //Goes back to Main Screen.
+                            val intent = Intent(this@DetailedView, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    ) {
+                        Text("Back To Base")
                     }
-                ) {
-                    Text("Back To Base")
                 }
             }
         }
--- app/src/main/java/com/example/campsitecommander/WelcomeScreen.kt
+++ app/src/main/java/com/example/campsitecommander/WelcomeScreen.kt
@@ -7,12 +7,15 @@ import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.os.postDelayed
@@ -24,22 +27,30 @@ class WelcomeScreen : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CampsiteCommanderTheme {
-                Text("Welcome to the app which helps you store the camping gear and food supplies you will need for the camp.")
+                Column(
+                    modifier =
+                        Modifier.fillMaxSize(),
+                    verticalArrangement =
+                        Arrangement.Center,
+                    horizontalAlignment =
+                        Alignment.CenterHorizontally
+                ) {
+                    Text("Welcome to the app which helps you store the camping gear and food supplies you will need for the camp.")
 
-                Handler(Looper.getMainLooper()).postDelayed({
-                    //starts the next screen.
-                    val intent = Intent(this@WelcomeScreen, MainActivity::class.java)
-                    startActivity(intent)
-                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-                    finish()
-                } , 3000) // changes to main screen after 3000 milliseconds(3 seconds).
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        //starts the next screen.
+                        val intent = Intent(this@WelcomeScreen, MainActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                        finish()
+                    }, 3000) // changes to main screen after 3000 milliseconds(3 seconds).
 
 
+                }
             }
         }
+
     }
-
-
 }
 
 
