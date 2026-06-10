c85acec2a6e8922f4162666d9400bf3bcaef5923
tshegomahapa <tshegofatsomahapa19@gmail.com>
2026-06-10 09:26:02 +0200

    initial commit

--- app/src/main/java/com/example/campsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/campsitecommander/MainActivity.kt
@@ -19,29 +19,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CampsiteCommanderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CampsiteCommanderTheme {
-        Greeting("Android")
-    }
-}
